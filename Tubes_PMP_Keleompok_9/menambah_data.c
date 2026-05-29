14e9744ccaacce2014bb61a4b7f2edd4851973b9
Faiz Azmi Irwan <faizazmi.vasta21@gmail.com>
2026-05-29 10:38:22 +0700

    memperbaiki format struct sesuai global, penamaan function, dan comment

--- Tubes_PMP_Keleompok_9/menambah_data.c
+++ Tubes_PMP_Keleompok_9/menambah_data.c
@@ -1,141 +1,108 @@
-// A complete working C program to demonstrate 
-// all insertion methods on Linked List
+// Fungsi keseluruhan yang digunakan dalam menambah data inventaris
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "inventaris.h"
 
-// A linked list node
-struct Node
-{
-  int data;
-  struct Node *next;
-};
-
-// Given a reference (pointer to pointer) to 
-// the head of a list and an int, inserts a 
-// new node on the front of the list. 
-void push(struct Node** head_ref, 
-          int new_data)
-{
-    // 1. Allocate node 
-    struct Node* new_node = 
-           (struct Node*) malloc(sizeof(struct Node));
+// fungsi untuk menambahkan inventaris pada bagian awal list
+void menambah_data_awal(struct inventaris** head_ref, char* id, char* nama, 
+                        char* kategori, int stok, char* lokasi, char* status,
+                        char* pemilik, char* pic) {
+    // 1. Allocate inventaris 
+    struct inventaris* new_inventaris = 
+           (struct inventaris*) malloc(sizeof(struct inventaris));
 
     // 2. Put in the data  
-    new_node->data  = new_data;
+    strcpy(new_inventaris->id, id);
+    strcpy(new_inventaris->nama, nama);
+    strcpy(new_inventaris->kategori, kategori);
+    new_inventaris->stok = stok;
+    strcpy(new_inventaris->lokasi, lokasi);
+    strcpy(new_inventaris->status, status);
+    strcpy(new_inventaris->pemilik, pemilik);
+    strcpy(new_inventaris->pic, pic);
 
-    // 3. Make next of new node as head 
-    new_node->next = (*head_ref);
+    // 3. Make next of new inventaris as head 
+    new_inventaris->next = (*head_ref);
 
     // 4. move the head to point to 
-    //    the new node 
-    (*head_ref)    = new_node;
+    //    the new inventaris 
+    (*head_ref) = new_inventaris;
 }
 
-// Given a node prev_node, insert a 
-// new node after the given prev_node 
-void insertAfter(struct Node* prev_node, 
-                 int new_data)
-{
-    // 1. Check if the given prev_node 
+// fungsi untuk menambahkan inventaris setelah sebuah inventaris yang telah ditetapkan
+void menambah_data(struct inventaris* prev_inventaris, char* id, char* nama,
+                   char* kategori, int stok, char* lokasi, char* status,
+                   char* pemilik, char* pic) {
+    // 1. Check if the given prev_inventaris 
     //    is NULL 
-    if (prev_node == NULL)
+    if (prev_inventaris == NULL)
     {
-      printf("the given previous node cannot be NULL");
+      printf("the given previous inventaris cannot be NULL");
       return;
     }
 
-    // 2. Allocate new node 
-    struct Node* new_node =
-           (struct Node*) malloc(sizeof(struct Node));
+    // 2. Allocate new inventaris 
+    struct inventaris* new_inventaris =
+           (struct inventaris*) malloc(sizeof(struct inventaris));
 
     // 3. Put in the data  
-    new_node->data  = new_data;
+    strcpy(new_inventaris->id, id);
+    strcpy(new_inventaris->nama, nama);
+    strcpy(new_inventaris->kategori, kategori);
+    new_inventaris->stok = stok;
+    strcpy(new_inventaris->lokasi, lokasi);
+    strcpy(new_inventaris->status, status);
+    strcpy(new_inventaris->pemilik, pemilik);
+    strcpy(new_inventaris->pic, pic);
 
-    // 4. Make next of new node as next 
-    //    of prev_node 
-    new_node->next = prev_node->next;
+    // 4. Make next of new inventaris as next 
+    //    of prev_inventaris 
+    new_inventaris->next = prev_inventaris->next;
 
-    // 5. Move the next of prev_node 
-    //    as new_node 
-    prev_node->next = new_node;
+    // 5. Move the next of prev_inventaris 
+    //    as new_inventaris 
+    prev_inventaris->next = new_inventaris;
 }
 
-// Given a reference (pointer to pointer) to 
-// the head of a list and an int, appends a 
-// new node at the end  
-void append(struct Node** head_ref, 
-            int new_data)
-{
-    // 1. Allocate node 
-    struct Node* new_node = 
-           (struct Node*) malloc(sizeof(struct Node));
+// fungsi untuk menambahkan inventaris pada akhir dari list
+void menambah_data_akhir(struct inventaris** head_ref, char* id, char* nama,
+                         char* kategori, int stok, char* lokasi, char* status,
+                         char* pemilik, char* pic) {
+    // 1. Allocate inventaris 
+    struct inventaris* new_inventaris = 
+           (struct inventaris*) malloc(sizeof(struct inventaris));
 
     // Used in step 5
-    struct Node *last = *head_ref;  
+    struct inventaris *last = *head_ref;  
 
     // 2. Put in the data  
-    new_node->data  = new_data;
+    strcpy(new_inventaris->id, id);
+    strcpy(new_inventaris->nama, nama);
+    strcpy(new_inventaris->kategori, kategori);
+    new_inventaris->stok = stok;
+    strcpy(new_inventaris->lokasi, lokasi);
+    strcpy(new_inventaris->status, status);
+    strcpy(new_inventaris->pemilik, pemilik);
+    strcpy(new_inventaris->pic, pic);
 
-    // 3. This new node is going to be the 
-    //    last node, so make next of it as NULL
-    new_node->next = NULL;
+    // 3. This new inventaris is going to be the 
+    //    last inventaris, so make next of it as NULL
+    new_inventaris->next = NULL;
 
     // 4. If the Linked List is empty, then make 
-    //    the new node as head 
+    //    the new inventaris as head 
     if (*head_ref == NULL)
     {
-       *head_ref = new_node;
+       *head_ref = new_inventaris;
        return;
     }
 
-    // 5. Else traverse till the last node 
+    // 5. Else traverse till the last inventaris 
     while (last->next != NULL)
         last = last->next;
 
-    // 6. Change the next of last node 
-    last->next = new_node;
+    // 6. Change the next of last inventaris 
+    last->next = new_inventaris;
     return;
-}
-
-// This function prints contents of the 
-// linked list starting from head
-void printList(struct Node *node)
-{
-  while (node != NULL)
-  {
-     printf(" %d ", node->data);
-     node = node->next;
-  }
-}
-
-// Driver code
-int main()
-{
-  // Start with the empty list 
-  struct Node* head = NULL;
-
-  // Insert 6.  So linked list 
-  // becomes 6->NULL
-  append(&head, 6);
-
-  // Insert 7 at the beginning. 
-  // So linked list becomes 7->6->NULL
-  push(&head, 7);
-
-  // Insert 1 at the beginning. So 
-  // linked list becomes 1->7->6->NULL
-  push(&head, 1);
-
-  // Insert 4 at the end. So linked list 
-  // becomes 1->7->6->4->NULL
-  append(&head, 4);
-
-  // Insert 8, after 7. So linked list 
-  // becomes 1->7->8->6->4->NULL
-  insertAfter(head->next, 8);
-
-  printf("Created Linked list is: ");
-  printList(head);
-
-  return 0;
 }
