875d5241e16ad04ab505075000234ee95c97c86f
Faiz Azmi Irwan <faizazmi.vasta21@gmail.com>
2026-05-29 10:38:56 +0700

    memperbaiki penamaan struct

--- Tubes_PMP_Keleompok_9/inventaris.h
+++ Tubes_PMP_Keleompok_9/inventaris.h
@@ -1,7 +1,7 @@
 #ifndef INVENTARIS_H
 #define INVENTARIS_H
 
-struct Node {
+struct inventaris {
     char id[15];        
     char nama[30];      
     char kategori[20];  
@@ -10,11 +10,11 @@ struct Node {
     char status[15];    
     char pemilik[30];   
     char pic[30];       
-    struct Node* next;  
+    struct inventaris* next;  
 };
 
-void memperbarui_status(struct Node* head, char* target_id, char* status_baru);
-void menampilkan_seluruh_data(struct Node* head);
-void menampilkan_ringkasan(struct Node* head);
+void memperbarui_status(struct inventaris* head, char* target_id, char* status_baru);
+void menampilkan_seluruh_data(struct inventaris* head);
+void menampilkan_ringkasan(struct inventaris* head);
 
 #endif
