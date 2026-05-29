3965868ed569e4a4ea8883f755cb020b7209cb6f
Hezkiel Tarutung Harianja <hezkiel.t.harianja@gmail.com>
2026-05-29 11:02:05 +0700

    menambahkan comment pada kode

--- Tubes_PMP_Keleompok_9/memperbarui_status_tubes.c
+++ Tubes_PMP_Keleompok_9/memperbarui_status_tubes.c
@@ -4,18 +4,22 @@
 #include "inventaris.h"
 
 void memperbarui_status(struct inventaris* head, char* target_id, char* status_baru){
+    // cek apakah list kosong
     if(head == NULL){
         printf("Data inventaris masih kosong.\n");
         return;
     }
-
+    // pointer sementara
     struct inventaris* current = head;
-
+    // looping hingga ke akhir node
     while(current != NULL){
+        // kalau id dan targert_id sama,
+        // update statusnya dengan copy string status_baru ke status
         if(strcmp(current->id, target_id) == 0){
             strcpy(current->status, status_baru);
             return;
         }
+        // menggeser pointer ke node berikutnya
         current = current->next;
     }
     
