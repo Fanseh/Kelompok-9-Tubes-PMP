18faf7ee052a3d7bfbd913b737b1cc8930767d1a
kanessicleo <kanessicleo@gmail.com>
2026-05-29 10:06:15 +0700

    memperbarui inventaris.h dengan deklarasi fungsi cari data dan update stok

--- Tubes_PMP_Keleompok_9/inventaris.h
+++ Tubes_PMP_Keleompok_9/inventaris.h
@@ -16,5 +16,7 @@ struct Node {
 void memperbarui_status(struct Node* head, char* target_id, char* status_baru);
 void menampilkan_seluruh_data(struct Node* head);
 void menampilkan_ringkasan(struct Node* head);
+void mencari_data_berdasarkan_id(Node* cari_head, const char* id_cari);
+void memperbarui_stok(Node* stok_head, const char* stok_id_cari, int stok_perubahan);
 
 #endif
